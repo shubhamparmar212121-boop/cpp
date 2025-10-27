88e3e2b3b49bfcf92d2354ce82d3de09f0e78d35
shubhamparmar212121-boop <shubhamparmar212121@gmail.com>
2025-10-27 10:10:18 +0530

    add

---
+++ stringprintonlyalphabets.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    string temp = "";
+    cout<<"Enter a string : ";
+    getline(cin,str);
+    for(int i = 0; i <= str.size(); i++){
+        if((str[i]>='a' && str[i] <='z' ) || (str[i]))
+    }
+
+}
